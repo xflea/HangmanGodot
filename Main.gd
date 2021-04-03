a1fcb2776e04d832d57a0b0c8a1cd7de501d1406
Mirko Colangeli <kolaflea93@gmail.com>
2021-04-04 01:01:48 +0200

    tastiera e sistema di gioco completi
    manca solo il mettere a schermo la parola mano a mano che vengono indovinate le parole

--- Main.gd
+++ Main.gd
@@ -12,7 +12,10 @@ var lettere_indovinate = []
 var errori = 0
 var max_errori = 5
 
+var estratta = ""
 var estratta_a_schermo = ""
+
+var gioco_finito = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,15 +25,10 @@ func _ready():
 		if lettera == "_":
 			estratta_a_schermo += " "
 		estratta_a_schermo += "_ "
-	
-	var nome_immagine = "res://images/sol/" + String(estratta) + ".png"
-	var immagine_da_mettere = load(nome_immagine)
-	get_node("img_da_indovinare").texture = immagine_da_mettere
 
 
 func estrai(array):
 	var rng = RandomNumberGenerator.new()
-	var estratta = ""
 	rng.randomize()
 	var my_random_number = rng.randf_range(0, array.size())
 	estratta = array[my_random_number]
@@ -44,18 +42,30 @@ func estrai(array):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	get_node("label_da_indovinare").text = estratta_a_schermo
-	get_node("label_errori").text = String(errori) + "/" + String(max_errori)
 
 func controlla_lettera(lettera):
 	lettera = String(lettera)
 	if lettere_da_indovinare.has(lettera):
 		lettere_da_indovinare.erase(lettera)
 		lettere_indovinate.append(lettera)
+		if lettere_da_indovinare.empty():
+			gioco_finito = true
+			get_node("label_errori").text = "EVVIVA!!!"
+			var nome_immagine = "res://images/sol/" + String(self.estratta) + ".png"
+			var immagine_da_mettere = load(nome_immagine)
+			get_node("img_da_indovinare").texture = immagine_da_mettere
+
 	else:
 		errori += 1
-		var nome_immagine = "res://images/hangman/" + String(errori) + ".png"
-		var immagine_da_mettere = load(nome_immagine)
-		get_node("img_impiccato").texture = immagine_da_mettere
+		if errori < max_errori:
+			var nome_immagine = "res://images/hangman/" + String(errori) + ".png"
+			var immagine_da_mettere = load(nome_immagine)
+			get_node("label_errori").text = String(errori) + "/" + String(max_errori)
+			get_node("img_impiccato").texture = immagine_da_mettere
+		else:
+			gioco_finito = true
+			get_node("label_errori").text = "GAME OVER"
+			get_node("img_impiccato").texture = load("res://images/hangman/6.png")
 
 # tutti bottoni da qui sotto
 
@@ -63,49 +73,176 @@ func _on_btn_reset_pressed():
 	get_tree().reload_current_scene()
 
 func _on_btn_a_pressed():
-	get_node("btn_a").disabled = true
-	controlla_lettera("a")
+	if !gioco_finito:
+		get_node("btn_a").disabled = true
+		controlla_lettera("a")
 
 func _on_btn_b_pressed():
-	get_node("btn_b").disabled = true
-	controlla_lettera("b")
+	if !gioco_finito:
+		get_node("btn_b").disabled = true
+		controlla_lettera("b")
 
 func _on_btn_q_pressed():
-	get_node("btn_q").disabled = true
-	controlla_lettera("q")
+	if !gioco_finito:
+		get_node("btn_q").disabled = true
+		controlla_lettera("q")
 
 func _on_btn_w_pressed():
-	get_node("btn_w").disabled = true
-	controlla_lettera("w")
+	if !gioco_finito:
+		get_node("btn_w").disabled = true
+		controlla_lettera("w")
 
 func _on_btn_e_pressed():
-	get_node("btn_e").disabled = true
-	controlla_lettera("e")
+	if !gioco_finito:
+		get_node("btn_e").disabled = true
+		controlla_lettera("e")
 
 func _on_btn_r_pressed():
-	get_node("btn_r").disabled = true
-	controlla_lettera("r")
+	if !gioco_finito:
+		get_node("btn_r").disabled = true
+		controlla_lettera("r")
 
 func _on_btn_t_pressed():
-	get_node("btn_t").disabled = true
-	controlla_lettera("t")
+	if !gioco_finito:
+		get_node("btn_t").disabled = true
+		controlla_lettera("t")
 
 func _on_btn_y_pressed():
-	get_node("btn_y").disabled = true
-	controlla_lettera("y")
+	if !gioco_finito:
+		get_node("btn_y").disabled = true
+		controlla_lettera("y")
 
 func _on_btn_u_pressed():
-	get_node("btn_u").disabled = true
-	controlla_lettera("u")
+	if !gioco_finito:
+		get_node("btn_u").disabled = true
+		controlla_lettera("u")
 
 func _on_btn_i_pressed():
-	get_node("btn_i").disabled = true
-	controlla_lettera("i")
+	if !gioco_finito:
+		get_node("btn_i").disabled = true
+		controlla_lettera("i")
 
 func _on_btn_o_pressed():
-	get_node("btn_o").disabled = true
-	controlla_lettera("o")
+	if !gioco_finito:
+		get_node("btn_o").disabled = true
+		controlla_lettera("o")
 
 func _on_btn_p_pressed():
-	get_node("btn_p").disabled = true
-	controlla_lettera("p")
+	if !gioco_finito:
+		get_node("btn_p").disabled = true
+		controlla_lettera("p")
+
+func _on_btn_s_pressed():
+	if !gioco_finito:
+		get_node("btn_s").disabled = true
+		controlla_lettera("s")
+
+func _on_btn_d_pressed():
+	if !gioco_finito:
+		get_node("btn_d").disabled = true
+		controlla_lettera("d")
+
+func _on_btn_f_pressed():
+	if !gioco_finito:
+		get_node("btn_f").disabled = true
+		controlla_lettera("f")
+
+func _on_btn_g_pressed():
+	if !gioco_finito:
+		get_node("btn_g").disabled = true
+		controlla_lettera("g")
+
+func _on_btn_j_pressed():
+	if !gioco_finito:
+		get_node("btn_j").disabled = true
+		controlla_lettera("j")
+
+func _on_btn_h_pressed():
+	if !gioco_finito:
+		get_node("btn_h").disabled = true
+		controlla_lettera("h")
+
+func _on_btn_k_pressed():
+	if !gioco_finito:
+		get_node("btn_k").disabled = true
+		controlla_lettera("k")
+
+func _on_btn_l_pressed():
+	if !gioco_finito:
+		get_node("btn_l").disabled = true
+		controlla_lettera("l")
+
+func _on_btn_z_pressed():
+	if !gioco_finito:
+		get_node("btn_z").disabled = true
+		controlla_lettera("z")
+
+func _on_btn_x_pressed():
+	if !gioco_finito:
+		get_node("btn_x").disabled = true
+		controlla_lettera("x")
+
+func _on_btn_c_pressed():
+	if !gioco_finito:
+		get_node("btn_c").disabled = true
+		controlla_lettera("c")
+
+func _on_btn_v_pressed():
+	if !gioco_finito:
+		get_node("btn_v").disabled = true
+		controlla_lettera("v")
+
+func _on_btn_n_pressed():
+	if !gioco_finito:
+		get_node("btn_n").disabled = true
+		controlla_lettera("n")
+
+func _on_btn_m_pressed():
+	if !gioco_finito:
+		get_node("btn_m").disabled = true
+		controlla_lettera("m")
+
+func _on_btn_1_pressed():
+	if !gioco_finito:
+		get_node("btn_1").disabled = true
+		controlla_lettera("1")
+
+func _on_btn_2_pressed():
+	if !gioco_finito:
+		get_node("btn_2").disabled = true
+		controlla_lettera("2")
+
+func _on_btn_3_pressed():
+	if !gioco_finito:
+		get_node("btn_3").disabled = true
+		controlla_lettera("3")
+
+func _on_btn_4_pressed():
+	if !gioco_finito:
+		get_node("btn_4").disabled = true
+		controlla_lettera("4")
+
+func _on_btn_5_pressed():
+	if !gioco_finito:
+		get_node("btn_5").disabled = true
+		controlla_lettera("5")
+
+func _on_btn_6_pressed():
+	if !gioco_finito:
+		get_node("btn_6").disabled = true
+		controlla_lettera("6")
+
+func _on_btn_7_pressed():
+	if !gioco_finito:
+		get_node("btn_7").disabled = true
+		controlla_lettera("7")
+
+func _on_btn_8_pressed():
+	if !gioco_finito:
+		get_node("btn_8").disabled = true
+		controlla_lettera("8")
+
+func _on_btn_9_pressed():
+	if !gioco_finito:
+		get_node("btn_9").disabled = true
+		controlla_lettera("9")
